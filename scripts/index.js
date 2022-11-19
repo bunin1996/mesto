dbbade7c4fe72234a7f59732ec496f44af2dae0d
Aleksei Bunin <bunin1996@yandex.ru>
2022-11-19 19:48:31 +0300

    fix: исправить все ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,19 @@ const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__content');
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('#name');
-let jobInput = formElement.querySelector('#description');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_description');
 // Находим поля имени и описания
 let name = document.querySelector('.profile__name');
 let description = document.querySelector('.profile__description');
 // Присваиваем значениям полей формы содержимое полей имени
-nameInput.value = name.textContent;
-jobInput.value = description.textContent;
+
 
 
 const openPopup = function() {
   popupElement.classList.add('popup_is-opened');
+  nameInput.value = name.textContent;
+  jobInput.value = description.textContent;
 }
 
 const closePopup = function() {
@@ -26,8 +27,6 @@ function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   let nameInputValue = nameInput.value;
   let jobInputValue = jobInput.value;
-  let name = document.querySelector('.profile__name');
-  let description = document.querySelector('.profile__description');
   name.textContent = nameInputValue;
   description.textContent = jobInputValue;
   closePopup();
